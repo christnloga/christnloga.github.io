5b502fd90654cd805e5edc9f3b2470c3133469b5
christnloga <christ.nloga@gmail.com>
2025-12-30 16:16:35 +0100

    Sub-routes not found fix experimentation

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
 		<div className='p-6 h-screen flex justify-center items-center w-full'>
 			<Helmet>
 				<title>Let's Build Together In 2026</title>
-				<link rel="icon" type="image/svg+xml" href="/vite.svg" />
+				<link rel="icon" type="image/svg+xml" href="/my-photo.jpg" />
 				{/* <meta name='description' content='Beginner friendly page for learning React Helmet.' /> */}
 			</Helmet>
 			<h2 className="text-4xl font-bold">Let's Build Together In 2026</h2>
--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -6,7 +6,7 @@ const NotFound = () => {
         <div className='flex h-screen w-full justify-center items-center'>
             <Helmet>
                 <title>Let's Build Together In 2026</title>
-                <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+                <link rel="icon" type="image/svg+xml" href="/my-photo.jpg" />
                 {/* <meta name='description' content='Beginner friendly page for learning React Helmet.' /> */}
             </Helmet>
             <p className='text-black dark:text-white text-4xl font-bold'>404 Not Found</p>
